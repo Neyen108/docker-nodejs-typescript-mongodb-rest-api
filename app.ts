14b70b19993eca24576c40cb69d31b70d51f4fd4
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2021-07-13 18:06:45 +0530

    add helmet

--- app.ts
+++ app.ts
@@ -15,6 +15,7 @@ import { CommonRoutesConfig } from "./common/common.routes.config";
 import { UsersRoutes } from "./users/users.routes.config";
 import { AuthRoutes } from "./auth/auth.routes.config";
 import debug from "debug";
+import helmet from "helmet";
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
@@ -27,6 +28,8 @@ app.use(express.json());
 
 // here we are adding middleware to allow cross-origin requests
 app.use(cors());
+
+app.use(helmet());
 
 // here we are preparing the expressWinston logging middleware configuration,
 // which will automatically log all HTTP requests handled by Express.js
